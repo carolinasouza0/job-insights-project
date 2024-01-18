777d7a84bc1a9d497837dfd7683475da85b9716c
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-01-18 18:29:09 -0300

    test: implement Count Ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+    word = "FULL-TIME"
+    result = count_ocurrences(path, word)
+    assert result == 726
