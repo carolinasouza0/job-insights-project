83fa17a8992f2eb2ca3faae2681772374e984597
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-01-20 11:32:06 -0300

    feat: implement read method

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        self.jobs_list = list()
+        with open(path, "r", encoding="utf-8") as file:
+            open_file = csv.DictReader(file, delimiter=",", quotechar='"')
+            self.jobs_list = [row for row in open_file]
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
