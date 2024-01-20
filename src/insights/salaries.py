7df2d703671f6847803b0bb9e33519e71b592d0e
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-01-20 12:24:43 -0300

    feat: implement get_max_salary method

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,7 +7,14 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        salary_list = [
+            int(job["max_salary"])
+            for job in self.jobs_list
+            if job["max_salary"].isnumeric()
+        ]
+        highest_salary = max(salary_list)
+
+        return highest_salary
 
     def get_min_salary(self) -> int:
         pass
