5090d6615dd9a3213f98953d9db9420f45ab1cde
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-01-20 12:38:09 -0300

    feat: implement matches_salary_range method

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -27,7 +27,31 @@ class ProcessSalaries(ProcessJobs):
         return lowest_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError(
+                "Dicionário de job deve conter min_salary e max_salary"
+            )
+        if (
+            not str(job["min_salary"]).isnumeric()
+            or not str(job["max_salary"]).isnumeric()
+        ):
+            raise ValueError(
+                "Os valores de min_salary e max_salary devem ser numéricos."
+            )
+
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+
+        if min_salary > max_salary:
+            raise ValueError("min_salary deve ser menor que max_salary")
+        if not (
+            isinstance(salary, int)
+            or (isinstance(salary, str))
+            and salary.isnumeric()
+        ):
+            raise ValueError("salary deve ser um valor numérico")
+
+        return min_salary <= int(salary) <= max_salary
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
